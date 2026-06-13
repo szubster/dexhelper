d17d1f5545eadedc47bc5610e362b4c69c39e267
Tomasz Szuba <szubster@gmail.com>
2026-06-13 23:17:31 +0200

    Delete commit_fix.sh

--- commit_fix.sh
+++
@@ -1,1 +0,0 @@
-git add src/engine/saveParser/parsers/common.ts src/engine/saveParser/parsers/gen3.ts src/engine/saveParser/parsers/gen3.test.ts .foundry/tasks/task-095-157-gen3-berry-dataview-parsing.md
