ef79e4ba75c74215f137a6ce9cf67f7052354e3f
Tomasz Szuba <szubster@gmail.com>
2026-06-25 06:51:07 +0200

    Delete fix_test.py

--- fix_test.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('src/components/__tests__/SearchAndFilters.test.tsx', 'r') as f:
-    content = f.read()
-
-content = content.replace("[ FILTER_PARAMETERS ]", "[ PARAMETER_MATRIX ]")
-
-with open('src/components/__tests__/SearchAndFilters.test.tsx', 'w') as f:
-    f.write(content)
