1d00b9a8aa122a39b1cdddee75618e587408219f
Tomasz Szuba <szubster@gmail.com>
2026-07-05 18:31:10 +0200

    Delete list_all_md.py

--- list_all_md.py
+++
@@ -1,8 +0,0 @@
-import os
-files = []
-for root, dirs, filenames in os.walk('.foundry/docs'):
-    for filename in filenames:
-        if filename.endswith('.md'):
-            files.append(os.path.join(root, filename))
-for f in sorted(files):
-    print(f)
