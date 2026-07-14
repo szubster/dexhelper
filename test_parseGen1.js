8f4f7b3e6f6b54bd4a0e20e80b3b967a88513eca
Tomasz Szuba <szubster@gmail.com>
2026-07-14 20:35:34 +0200

    Delete test_parseGen1.js

--- test_parseGen1.js
+++
@@ -1,3 +0,0 @@
-import { readFileSync } from 'fs';
-const file = readFileSync('src/engine/saveParser/parsers/common.ts', 'utf-8');
-console.log(file.split('\n').findIndex(line => line.includes('gen3FeebasTiles?: number[];')));
