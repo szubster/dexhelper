99847aa537044336f54baf02f448540d4a067163
Tomasz Szuba <szubster@gmail.com>
2026-07-16 23:03:46 +0200

    Delete fix_story.js

--- fix_story.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const content = fs.readFileSync('.foundry/stories/story-090-134-garbage-collection-integration.md', 'utf-8');
-const newContent = content.replace('updated_at: \'2026-07-16\'', `updated_at: '${new Date().toISOString().split('T')[0]}'`);
-fs.writeFileSync('.foundry/stories/story-090-134-garbage-collection-integration.md', newContent);
