74668ad94c048be2c769cee41679f422d006e494
Tomasz Szuba <szubster@gmail.com>
2026-08-03 14:23:04 +0200

    Delete test-review.js

--- test-review.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-console.log(fs.existsSync('.foundry/stories/story-097-261-extract-pokemon-met-locations.md'));
-console.log(fs.existsSync('.foundry/stories/story-097-262-aggregate-first-catch-by-route.md'));
-console.log(fs.existsSync('.foundry/stories/story-097-263-flag-nuzlocke-route-violations.md'));
