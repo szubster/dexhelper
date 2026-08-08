e4daba4b98ea458dffcfc606060656a8343c53fa
Tomasz Szuba <szubster@gmail.com>
2026-08-08 15:46:47 +0200

    Delete fix_test.js

--- fix_test.js
+++
@@ -1,5 +0,0 @@
-import fs from 'fs';
-const testPath = 'src/engine/saveParser/gen3/trainerFlags/parser.test.ts';
-let code = fs.readFileSync(testPath, 'utf-8');
-code = code.replace(/const buffer = new ArrayBuffer\(4000\);/, 'const buffer = new ArrayBuffer(5000);');
-fs.writeFileSync(testPath, code);
