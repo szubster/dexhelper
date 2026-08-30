b378bfa89a051c3f6aad48d815b0ed9e02fdb740
Tomasz Szuba <szubster@gmail.com>
2026-08-30 19:14:03 +0200

    Delete fix_test.cjs

--- fix_test.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-let content = fs.readFileSync('src/engine/saveParser/parsers/gen3_extraction.test.ts', 'utf8');
-
-// The replacement above failed or duplicated SUBSTRUCTURE_ORDER, let's fix it by parsing properly.
-content = content.replace("  SUBSTRUCTURE_ORDER,\n", "");
-
-fs.writeFileSync('src/engine/saveParser/parsers/gen3_extraction.test.ts', content);
