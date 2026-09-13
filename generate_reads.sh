4fd2590a1bc35146973458b7420ad6f9eb805467
Tomasz Szuba <szubster@gmail.com>
2026-09-14 00:01:31 +0200

    Delete generate_reads.sh

--- generate_reads.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find .foundry/docs -type f -name "*.md" | while read -r file; do
-    echo "call:default_api:read_file{filepath:\"$file\"}"
-done
-find .foundry/archive/docs/adrs -type f -name "*.md" | while read -r file; do
-    echo "call:default_api:read_file{filepath:\"$file\"}"
-done
