4ec4bb6374f1fbdff91bc38caa5b42d10713f901
Tomasz Szuba <szubster@gmail.com>
2026-09-14 00:01:39 +0200

    Delete generate_reads_tasks.sh

--- generate_reads_tasks.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-find .foundry/tasks -type f -name "task-536-565-core-policies-parent-nodes-update.md" | while read -r file; do
-    echo "call:default_api:read_file{filepath:\"$file\"}"
-done
